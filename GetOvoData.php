303b5614f85aa7fdfae2bee635807bf6d2493dc5
Paul Adams <51832600+ThePaulAdams@users.noreply.github.com>
2020-02-24 17:10:13 +0000

    Update and rename backfill.php to GetOvoData.php

--- GetOvoData.php
+++ GetOvoData.php
@@ -45,7 +45,9 @@ curl_setopt($ch, CURLOPT_COOKIEJAR,  $cookie_file);
 curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_file);
 curl_setopt($ch, CURLOPT_COOKIESESSION, true );
 //login to OVO online and visit https://smartpaym.ovoenergy.com/api/customer-and-account-ids to get account ID for below
+//after backfilling data replace with line below and run on CRON Job daily
 curl_setopt($ch, CURLOPT_URL ,"https://smartpaym.ovoenergy.com/api/energy-usage/half-hourly/*****?date=YYYY-MM-DD");  
+//curl_setopt($ch, CURLOPT_URL ,"https://smartpaym.ovoenergy.com/api/energy-usage/half-hourly/6422168?date=".$date); 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER , true);
 curl_setopt($ch, CURLOPT_ENCODING , "");
 curl_setopt($ch, CURLOPT_MAXREDIRS , 10);
